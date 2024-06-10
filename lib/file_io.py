e6261b27c7b44ca242a1dad50f5f508c336fee36
Njagi Githaiga <njagigithaiga@gmail.com>
2024-06-11 02:01:27 +0300

    Complete lab

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,27 @@
 def write_file(file_name, file_content):
     pass
+    with open(f"{file_name}.txt", "w") as t:
+        t.write(file_content)
 
 def append_file(file_name, append_content):
     pass
+    with open(f"{file_name}.txt", "a") as t:
+        t.write(append_content)
 
 def read_file(file_name):
     pass
+    with open(f"{file_name}.txt") as t:
+        return t.read()
+
+
+# use write_file function. 
+write_file(file_name="logfile", file_content="Log 1: 5 bananas added" )
+
+read_file(file_name="logfile")
+print(read_file(file_name="logfile"))
+
+# use append_file function.
+append_file(file_name="logfile", append_content="Log 2: 3 bananas subtracted")
+
+read_file(file_name="logfile")
+print(read_file(file_name="logfile"))
